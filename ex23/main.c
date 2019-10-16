12cf4ddf7d7a7d06369830c69b1043cf213d2617
Markus Ahola <mahola@c3r5p4.hive.fi>
2019-10-16 17:45:21 +0300

    Initial upload

---
+++ ex23/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mahola <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 13:53:13 by mahola            #+#    #+#             */
+/*   Updated: 2019/10/14 14:03:35 by mahola           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_point.h"
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int		main(void)
+{
+	t_point point;
+
+	set_point(&point);
+	printf("X: %d, Y: %d", point.x, point.y);
+	return (0);
+}
